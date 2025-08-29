a261a1a3a6d2a35dcece927be12e007fc711a2f9
christianzaboli <zabolichristian@gmail.com>
2025-08-29 17:46:38 +0200

    Step 02 Bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -17,7 +17,8 @@ console.log("Questa é l'array con l'ordine inverto dei propri elementi");
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames =
+const longNames = teachers.filter(words => words.length >= 5);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
